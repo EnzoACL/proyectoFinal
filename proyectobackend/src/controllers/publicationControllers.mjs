9e9477752f0d50cbfcc8237eecf2b190814f843c
EnzoACL <afdm@postal6.academiapostal.es.local>
2022-05-12 13:00:09 +0200

    Delete posts controller done and tested with insomia

--- proyectobackend/src/controllers/publicationControllers.mjs
+++ proyectobackend/src/controllers/publicationControllers.mjs
@@ -3,13 +3,27 @@ import db from "../models/dataBase.mjs"
 export function postPublicationController(request, response) {
     const { idfromuser, content } = request.body;
     db.run(
-        `INSERT INTO posts(content) VALUES ("${idfromuser}","${content}")`,
-        (err) => {
+        `INSERT INTO posts(idfromuser ,content) VALUES ("${idfromuser}","${content}")`,
+        (err)=> {
             if (err) {
                 console.error(err);
                 response.sendStatus(500)
             } else {
                 response.sendStatus(201)
+            }
+        }
+    )
+}
+export function deletePublicationController(request, response) {
+    const { idfromuser, id } = request.body;
+    db.run(
+        `DELETE FROM posts WHERE idfromuser=("${idfromuser}") AND id=("${id}")`,
+        (err) => {
+            if (err) {
+                console.error(err);
+                response.sendStatus(200)
+            } else {
+                response.sendStatus(202)
             }
         }
     )
