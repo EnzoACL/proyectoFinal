31dc9a734bea3b52a24039d243e66560efb49cbf
EnzoACL <afdm@postal6.academiapostal.es.local>
2022-05-31 12:40:08 +0200

    Half done posts and comments

--- proyetofrontend/src/components/Post/Posts.jsx
+++ proyetofrontend/src/components/Post/Posts.jsx
@@ -1,30 +1,50 @@
 import { useContext, useState } from "react";
 import UserData from "../UserData/UserData";
 import Buttons from "../ButtonsRRSS/Buttons";
-import Comments from "../Comments/Comments";
+import ShowComments from "../Comments/ShowComments";
 import { get } from '../../aux_api'
 function Posts({ userId }) {
     const [listOfPost, setListOfPost] = useState("")
-    const arrayOfPost = []
+    //const arrayOfPost = []
 
     async function getPostOf(){
         const postsOfUser = await get(`http://localhost:4000/name/V0.0/publicationsofuser/${userId}`)
-        
-        for (let item in postsOfUser) {
-            arrayOfPost.push(item)
+        console.log(postsOfUser);
+        const arrayOfPost = postsOfUser.map(
+            item => item.content
+        )
+        const postId = postsOfUser.map(
+            item => item.id
+        )
+        console.log(postId)
+        const arrayPostsTest = []
+
+        function constructPosts() {
+            let idx = 0
+            while (arrayPostsTest.length < (arrayOfPost.length * 2)) {                
+                arrayPostsTest.push(arrayOfPost[idx])
+                arrayPostsTest.push(postId[idx])
+                idx++
+            }
+            console.log(arrayPostsTest);
         }
-        setListOfPost(<p>
+        constructPosts();
+       
+   
+      /* setListOfPost(<p>
             {arrayOfPost.map((arrayOfPost) => (
                 <>
                     <ul>
                     {arrayOfPost}
-                    <Buttons></Buttons>
-                    <Comments></Comments>
+                    <p><Buttons></Buttons></p>
+                    <p><ShowComments postId={postId} /></p>
                     </ul>
                 </>
             
-            ))}
-        </p>)
+                )
+             )
+           }
+       </p>)*/
     }
     
     return (
