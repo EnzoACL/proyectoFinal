9689ea9fea1f1a6957e9f5d4d0be556e716fbf21
EnzoACL <afdm@postal6.academiapostal.es.local>
2022-06-06 12:30:48 +0200

    Search bar done

---
+++ proyetofrontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { get } from '../../aux_api';
+
+
+//Barra de busqueda. Se hace un get de los usuarios y se filtra por nombre.
+function SearchBar() {
+    const [userList, setUserList] = useState("");
+    const [userNameOf, setUserNameOf] = useState("");
+    
+    const getUserNameOf = (event) => {
+        setUserNameOf(event.target.value)
+    }
+
+    /**
+     * 
+     * @param {array} item 
+     * @returns Nombres que coinciden con lo que hay en el input.
+     */
+    function filterFunction(item) {
+         if (item.includes(userNameOf)) {
+            return item
+        }
+       
+    }
+    
+    async function startSearch() {
+        const users = await get("http://localhost:4000/name/V0.0/users/");
+        const usersNames = users.map(
+            (item) => item.name
+        );
+        const filteredUsers = usersNames.filter(filterFunction)
+        const arrangedUsersSearch = filteredUsers.map((item) => <ul>{item}</ul>)
+        setUserList(arrangedUsersSearch);      
+    }
+
+    return (
+        <>
+            <h1>Ejemplo de buscador</h1>
+            <input placeholder="Buscar..." onChange={getUserNameOf}/>
+            <input type="button" value="🔍" onClick={startSearch} />
+            <ul>{userList}</ul>
+        </>
+    )
+}
+export default SearchBar
