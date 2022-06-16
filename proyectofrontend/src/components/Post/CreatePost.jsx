94d6cbbf8b6bee1abd1c4f5192f8367d5dc237f4
EnzoACL <afdm@postal6.academiapostal.es.local>
2022-06-16 11:12:01 +0200

    Create post changed and working

--- proyectofrontend/src/components/Post/CreatePost.jsx
+++ proyectofrontend/src/components/Post/CreatePost.jsx
@@ -7,55 +7,39 @@ function CreatePost() {
     const data = useContext(Context);
     const [postInterface, setPostInterface] = useState("")
     const [postBody, setPostBody] = useState("")
-   
+    
+    
     const getPostContent = (event) => {
         setPostBody(event.target.value)
     }
     
-    useEffect(
-        () => console.log("test"), []
-    )
     
-  
-
+    
     function postPublication() {
         const publicationReady = JSON.stringify({idfromuser: data.userId, content: postBody });
         console.log(publicationReady);
-        //const url = "http://localhost:4000/name/V0.0/publication/"
+        const url = "http://localhost:4000/name/V0.0/publication/"
         //Aqui usamos el context para hacer el post con aut
-        //const tokenPost = authToken(data.user, data.password)
-       // authPost(url,tokenPost,publicationReady)
+        const tokenPost = authToken(data.user, data.password)
+        authPost(url,tokenPost,publicationReady)
     }
 
-    function showCreatePost() {
-        if (postInterface === "") {
-            setPostInterface(
-                <>  
-                                        
-                        <p>Contenido del post</p>
-                        <p>
-                            <textarea type="text" id="postContent" name="postContent" onChange={getPostContent} />
-                        </p>
-                        <p>
-                            
-                            <input type="button" value="Publicar post" onClick={postPublication} />
-                        </p>
-                    
-                    
-                    
-                </>
-            )
-        } else {setPostInterface("")}
-        
-        
-
-    }
+    
     
     return (
-        <>
-            <h1>Crear post</h1>
-            <input type="button" value="Crear post" onClick={showCreatePost} />
-            {postInterface}
+        <>  
+                                        
+            <p>Crear post</p>
+            <p>
+                <textarea type="text" id="postContent" name="postContent" onChange={getPostContent} />
+            </p>
+            <p>
+                
+                <input type="button" value="Publicar post" onClick={postPublication} />
+            </p>
+                    
+                    
+                    
         </>
     )
 }
